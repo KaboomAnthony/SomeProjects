4c93872995119402a7f4cbfa74f6e158d89d14c8
KaboomAnthony <kaboom.anthony@hotmail.com>
2020-06-24 18:03:34 +0700

    first lesson

--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -3,8 +3,32 @@ public class Loader
 {
     public static void main(String[] args)
     {
-        Cat cat = new Cat();
+        Cat murkaCat = new Cat();
+        System.out.println("Weight your cat murka: " + murkaCat.getWeight());
+        while (!(murkaCat.getStatus().equals("Dead"))) {
+            murkaCat.meow();
+        }
+        System.out.println("Your cat murka: " + murkaCat.getStatus());
 
-        System.out.println(cat.getStatus());
+        Cat vasyaCat = new Cat();
+
+        double servingSize = 10;
+        double drinkSize = 5;
+
+        int foodCounter = 0;
+
+        while (!(vasyaCat.getStatus().equals("Exploded"))) {
+            vasyaCat.feed(servingSize);
+            System.out.println("Your cat Vasya ate food and its weight: " + vasyaCat.getWeight());
+
+            foodCounter++;
+            if (foodCounter == 10) {
+                vasyaCat.drink(drinkSize);
+                System.out.println("Your cat Vasya drank water and its weight: " + vasyaCat.getWeight());
+
+                foodCounter = 0;
+            }
+        }
+        System.out.println("Your cat vasya: " + vasyaCat.getStatus());
     }
 }
